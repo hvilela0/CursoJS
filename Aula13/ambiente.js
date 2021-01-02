a760b1fb347f6601509f7bfb9c66812cca2a303f
hvilela0 <39452510+hvilela0@users.noreply.github.com>
2021-01-02 06:06:05 -0300

    nova aula (incluidos aula 13 e 14)

---
+++ Aula13/ambiente.js
@@ -0,0 +1,16 @@
+let i=1;
+
+/*while(i<=20){
+    console.log(`Passo ${i}`);
+    i++;
+}*/
+/*
+do{
+    console.log(`Passo ${i}`);
+}while(++i<=10);
+*/
+
+for(i=1;i<=4;i++){
+    console.log(`Passo ${i}`);
+
+}
